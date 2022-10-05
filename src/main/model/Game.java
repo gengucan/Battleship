9c299a4615cdbb05fc9883ed3ee04eb86dc3c0ef
James Lee <jameslee8791@gmail.com>
2022-10-04 19:39:18 -0700

    filled out classes more; still incomplete

--- src/main/model/Game.java
+++ src/main/model/Game.java
@@ -1,9 +1,49 @@
 package model;
 
 public class Game {
-    Board board1 = new Board();
-    Board board2 = new Board();
+    //private int turnSelect = 0;
 
+    public Game() { // main creates an instance of game?
+        int turnSelect = 0;
 
+        Board board1 = new Board();
+        Board board2 = new Board();
+
+        Player player1 = new Player();
+        Player player2 = new Player();
+
+    }
+
+    public void playerTurn(Player p) {
+        p.select();
+
+    }
+
+    public void nextPlayerTurn(int turn, Player p1, Player p2) {
+        if ((turn % 2) != 0) {
+            p1.select();
+            ++turn;
+        } else if ((turn % 2) == 0) {
+            p1.select();
+            ++turn;
+        }
+    }
+
+    //EFFECTS: Checks if either player has lost the game, if not start the next turn sequence
+    public void gameOver(int turn, Player p1, Player p2) { // 2 helper fn?
+        if (p1.getIsGameOver() == true) {
+            end(p2);
+        } else if (p2.getIsGameOver() == true) {
+            end(p1);
+        } else {
+            nextPlayerTurn(turn, p1, p2); // need to find a way to preserve the turn counter
+        }
+    }
+
+    //
+    public void end(Player winner) {
+        System.out.println("The game is over, " + winner + "has won!");
+        // exit app
+    }
 
 }
--- src/main/model/Player.java
+++ src/main/model/Player.java
@@ -18,11 +18,17 @@ public class Player {
         }
     }
 
+    public boolean getIsGameOver() {
+        return isGameOver;
+    }
+
     public void select() {
         Scanner user = new Scanner(System.in);
-        System.out.println("Where would you like to scout?"); // add player num to the beginning
+        System.out.println("Where would you like to scout?"); // save this val
 
         String playerChoice = user.nextLine(); // need if statement next to check for which hit/miss lists to print
         //System.out.println(); // this should just print the hit and miss lists
+
+
     }
 }

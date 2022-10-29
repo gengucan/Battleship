c61a8d3225df79f442329527b82a6130a882c129
JLee <z0e0x@students.cs.ubc.ca>
2022-10-28 21:55:02 -0700

    Fixed coverage

--- src/test/persistence/JsonWriteTest.java
+++ src/test/persistence/JsonWriteTest.java
@@ -39,4 +39,22 @@ public class JsonWriteTest {
         }
     }
 
+    @Test
+    void testWriteSomeGame() {
+        try {
+            Board b = new Board();
+            JsonWrite jsonWrite = new JsonWrite("data/testWriteSomeGame.json");
+            b.addShip(1,0,0,0);
+            b.addShip(1,1,0,0);
+            jsonWrite.open();
+            jsonWrite.write(b);
+            jsonWrite.close();
+
+            JsonRead jsonRead = new JsonRead("data/testWriteNewGame.json");
+            b = jsonRead.read();
+            assertEquals(0, b.getShipsRemaining());
+        } catch (IOException e) {
+            fail("This exception should not be run");
+        }
+    }
 }
